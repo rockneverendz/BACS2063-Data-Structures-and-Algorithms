e744537fd5d3915698573609163295a9fa5b2169
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-12-23 05:24:58 +0800

    Create Hash.java
    works like a charm

---
+++ src/control/Hash.java
@@ -0,0 +1,25 @@
+package control;
+
+import java.util.Scanner;
+import model.Laptop;
+import util.HashMapImpl;
+import util.LinkedListImpl;
+
+public class Hash {
+
+    public static void main(String[] args) {
+        LinkedListImpl<Laptop> LaptopList = new MockData().getLinkedList();
+        HashMapImpl<String, Laptop> hashByName = new HashMapImpl<>();
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+
+        for (Laptop laptop : LaptopList) {
+            hashByName.put(laptop.getName(), laptop);
+        }
+        
+        System.out.print("Enter laptop model   : ");
+        String model = sc.nextLine();
+        
+        System.out.println(hashByName.get(model));
+        
+    }
+}
