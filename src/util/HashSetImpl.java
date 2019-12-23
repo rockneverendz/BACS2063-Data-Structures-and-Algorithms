6d7c7c31c886d3373f621f657e30e474717a8d4c
rockneverendz <31733763+rockneverendz@users.noreply.github.com>
2019-12-23 11:12:59 +0800

    Create HashSetImpl.java

---
+++ src/util/HashSetImpl.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.util.Iterator;
+
+public class HashSetImpl<E> implements ISet<E> {
+
+    private HashMapImpl<E, Object> map;
+
+    // Dummy value to associate with an Object in the backing Map
+    private static final Object PRESENT = new Object();
+
+    public HashSetImpl() {
+        map = new HashMapImpl<>();
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
